5171ddf44fa0bf5369d15bbd817aadc74cf97ce2
Labbi-Ahmed <labbi5689@gmail.com>
2023-03-07 16:30:28 +0600

    basic server test ok

--- server.js
+++ server.js
@@ -1,20 +1,8 @@
 const express = require('express');
-const errorHandler = require('./middleware/errorHandler');
-const dotenv = require('dotenv').config();
 const app = express();
 
-const port = process.env.PORT || 4001;
+const port = 5000;
 
-app.use(express.json());
-/// Main basic way to create a api
-// app.get("/api/contacts",(req,res)=>{
-//     res.status(200).json({message: "get the contacts"});
-// });
-/// this si create api commont for all /api/contacts and create fun /route/contactRoutes folder
-app.use("/api/contacts", require("./routes/contactRoutes"));
-
-app.use(errorHandler);
-
-app.listen(port, ()=>{
-    console.log(`server running on the poret ${port}`);
-});
+app.listen(port,(req,res)=>{
+    console.log("This is ok");
+})

1c62dfa19a0247a2312280e8b8fb5999e8552999
Labbi-Ahmed <labbi5689@gmail.com>
2023-03-07 17:21:29 +0600

    Commite

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const app = express();
 
 const port = 5000;
 
+app.get("/api/name", (req, res)=>{
+    res.send("my name is Labib");
+});
+
+app.post("/api/name", (req, res)=>{
+    res.statu("put the name :",req.body);
+});
 app.listen(port,(req,res)=>{
     console.log("This is ok");
 })

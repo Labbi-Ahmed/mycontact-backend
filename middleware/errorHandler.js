658d936e83b7ec80d93f8197565831263f4676da
Labbi-Ahmed <labbi5689@gmail.com>
2023-03-07 21:27:49 +0600

    temporary saved

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,4 +1,12 @@
 
-exports.errorHnader = (err,req,res,next)=>{
+const errorHnader = (err,req,res,next)=>{
+    const statusCode = res.statusCode? res.statusCode : 500;
+    switch (statusCode) {
+        case 400:
+            res.status
+            break;
     
+        default:
+            break;
+    }
 }
